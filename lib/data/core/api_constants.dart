96fca6380de74ce05a53f2a5037e230d1dd3d81a
vivpatel99 <vivpate99@gmail.com>
2021-02-15 18:03:11 +0100

    setup done

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  ApiConstants._(); // this class should not initiate from outside
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "4732cd3bba8bca2544b69d05e5eab4bc";
+
+  static const String BASE_IMAGE_URL = "https://api.themoviedb.org/t/p/w500";
+}
